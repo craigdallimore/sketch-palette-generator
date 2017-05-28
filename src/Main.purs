5857e4ce8754f1a4430d5dd52d7259b912f53061
craigdallimore <decoy9697@gmail.com>
2017-05-28 20:36:46 +1200

    Log on keyup

---
+++ src/Main.purs
@@ -0,0 +1,37 @@
+module Main where
+
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log)
+import DOM (DOM)
+import DOM.Event.Event (target)
+import DOM.Event.EventTarget (addEventListener, eventListener)
+import DOM.Event.Types (Event)
+import DOM.HTML (window)
+import DOM.HTML.Event.EventTypes (keyup)
+import DOM.HTML.Types (htmlDocumentToParentNode)
+import DOM.HTML.Window (document)
+import DOM.Node.Node (nodeValue)
+import DOM.Node.ParentNode (querySelector, QuerySelector(QuerySelector))
+import DOM.Node.Types (elementToEventTarget)
+import Data.Maybe (maybe)
+import Prelude
+
+-- Problems encountered
+-- 1. What do I need?
+-- 2. How do I access document.querySelector
+-- 3. how do I access document?
+-- 4. how do I create a QuerySelector ... oh, and it needs to be wrapped
+-- 5. the document is not a parentNode
+-- 6. how do I make an event listener
+-- 7. the element is a maybe element
+-- 8. the element is not an event target
+-- 9. How can I access e.target.value?
+
+f :: forall eff. Event -> Eff (dom :: DOM, console :: CONSOLE | eff) Unit
+f e = nodeValue (target e) >>= log
+
+main :: forall eff. Eff (dom :: DOM, console :: CONSOLE | eff) Unit
+main = do
+  doc <- window >>= document
+  el  <- querySelector (QuerySelector "#in") (htmlDocumentToParentNode doc)
+  maybe (log "No #in found!") (addEventListener keyup (eventListener f) true) (elementToEventTarget <$> el)
