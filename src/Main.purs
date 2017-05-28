598f48cdccc5153bb60802bacb807a3d3659127c
craigdallimore <decoy9697@gmail.com>
2017-05-29 08:41:22 +1200

    Tidy up

--- src/Main.purs
+++ src/Main.purs
@@ -3,18 +3,18 @@ module Main where
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import DOM (DOM)
+import DOM.Classy.Node (fromNode)
 import DOM.Event.Event (target)
 import DOM.Event.EventTarget (addEventListener, eventListener)
 import DOM.Event.Types (Event)
 import DOM.HTML (window)
 import DOM.HTML.Event.EventTypes (keyup)
+import DOM.HTML.HTMLTextAreaElement (value)
 import DOM.HTML.Types (HTMLTextAreaElement, htmlDocumentToParentNode)
 import DOM.HTML.Window (document)
-import DOM.HTML.HTMLTextAreaElement (value)
-import DOM.Classy.Node (fromNode)
 import DOM.Node.ParentNode (querySelector, QuerySelector(QuerySelector))
 import DOM.Node.Types (elementToEventTarget)
-import Data.Maybe (Maybe(Just), maybe)
+import Data.Maybe (maybe)
 import Prelude
 
 -- Problems encountered
@@ -29,14 +29,16 @@ import Prelude
 -- 9. How can I access e.target.value?
 
 f :: forall eff. Event -> Eff (dom :: DOM, console :: CONSOLE | eff) Unit
-f e = case (fromNode <<< target) e of
-  Just (ta :: HTMLTextAreaElement) -> do
-    val <- value ta
-    log val
-  _ -> log "?"
+f e = maybe
+  (log "No node found")
+  (\textarea -> value (textarea :: HTMLTextAreaElement) >>= log)
+  ((fromNode <<< target) e)
 
 main :: forall eff. Eff (dom :: DOM, console :: CONSOLE | eff) Unit
 main = do
   doc <- window >>= document
   el  <- querySelector (QuerySelector "#in") (htmlDocumentToParentNode doc)
-  maybe (log "No #in found!") (addEventListener keyup (eventListener f) true) (elementToEventTarget <$> el)
+  maybe
+    (log "No element #in found")
+    (addEventListener keyup (eventListener f) true)
+    (elementToEventTarget <$> el)
