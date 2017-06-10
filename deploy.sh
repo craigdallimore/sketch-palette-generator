638c0301977a1d9f9115b97a6e379a8cbf6cb13c
craigdallimore <decoy9697@gmail.com>
2017-06-11 10:41:28 +1200

    Minify javascript

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ git checkout master
 pulp build -O dist/main.js
 git checkout gh-pages
 node build-css.js
+./node_modules/uglify-js/bin/uglifyjs -c -m -o ./dist/main.min.js -- ./dist/main.js
 git add -f dist
 git commit -m "deploy"
 git push origin gh-pages
